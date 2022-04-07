a9d7437b4617916efc054322fbb998715202db9c
belekollietimothy2@gmail.com <belekollietimothy2@gmail.com>
2022-04-07 20:06:49 +0200

    myportfolio

---
+++ TimPortfolio/resources/views/components/home/learn-php.blade.php
@@ -0,0 +1,17 @@
+<x-call-to-action>
+  <x-slot:title>
+    <h4 class="text-white text-base font-semibold mb-2">
+     If you are interested in learing php check the Youtube Channel
+    </h4>
+    <h2 class=" text-white font-bold text-3xl sm:text-[38px] leading-tight mb-6 sm:mb-8 lg:mb-0 ">
+  If you want resources on <br class="hidden xs:block"/>
+     Laravel check me on github
+    </h2>
+  </x-slot>
+    <x-button-link href="https://github.com/TimothyBelekollie" variant="dark" class="mr-3" target="_blank">
+     Check me on Github
+    </x-button-link>
+    <x-button-link href="https://youtu.be/r9ndOH0tyfA" variant="red" target="_blank">
+      Watch on YouTube
+    </x-button-link>
+</x-call-to-action>
